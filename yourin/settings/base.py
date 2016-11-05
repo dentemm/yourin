b989b0660024bf1f4e1120a8931152f4f6a4bb26
Tim Claes <tim.claes@me.com>
2016-11-05 10:08:16 +0100

    secret key setting missing

--- yourin/settings/base.py
+++ yourin/settings/base.py
@@ -15,6 +15,8 @@ from __future__ import absolute_import, unicode_literals
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+SECRET_KEY = '!4e7cfxq_!(8g(*w^f5omk(!ox!z=-nrmh28m2y@ne1$)v=z_k'
+
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
