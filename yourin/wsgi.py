be440b4b1e43a4a902128f5a5cf8fe431ae6c651
Tim Claes <tim.claes@me.com>
2016-11-05 10:30:02 +0100

    wsgi file was not adjusted for production

--- yourin/wsgi.py
+++ yourin/wsgi.py
@@ -7,12 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-from __future__ import absolute_import, unicode_literals
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourin.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourin.settings.production")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
