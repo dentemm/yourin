0a744997c2897d4f0b61c326c036149165209c88
Tim Claes <tim.claes@me.com>
2016-12-30 18:29:38 +0100

    uploading to heroku

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourin.settings.dev")
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yourin.settings.production')
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourin.settings.dev")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yourin.settings.production')
 
     from django.core.management import execute_from_command_line
 
